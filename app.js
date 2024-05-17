4f68c528383190aae5f48acd1e2333071a06c079
VitorGSC <vitorgsc2004@gmail.com>
2024-05-17 10:54:42 -0300

    delete window location

--- app.js
+++ app.js
@@ -3,95 +3,92 @@ document.querySelector('#btnNav').addEventListener('click', event => {
     document.querySelector('#menuNav').classList.toggle('active');
 });
 
-if(window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/index.html#' || window.location.pathname === '/#' || window.location.pathname === 'FlashFlix/') {
-    console.log('index');
-    const apiKey = "bcc9273e9851f58686fedf85973c8de9";
-    const apiBaseUrl = "https://api.themoviedb.org/3";	
-    const language = document.documentElement.lang;
-    const nowPlayingUrlMovie = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}&language=${language}`;
-    const nowPlayingUrlSerie = `${apiBaseUrl}/tv/on_the_air?api_key=${apiKey}&language=${language}`;
-    const movieRatedUrl = `${apiBaseUrl}/movie/top_rated?api_key=${apiKey}&language=${language}`;
-    const serieRatedUrl = `${apiBaseUrl}/tv/top_rated?api_key=${apiKey}&language=${language}`;
-    const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
+const apiKey = "bcc9273e9851f58686fedf85973c8de9";
+const apiBaseUrl = "https://api.themoviedb.org/3";	
+const language = document.documentElement.lang;
+const nowPlayingUrlMovie = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}&language=${language}`;
+const nowPlayingUrlSerie = `${apiBaseUrl}/tv/on_the_air?api_key=${apiKey}&language=${language}`;
+const movieRatedUrl = `${apiBaseUrl}/movie/top_rated?api_key=${apiKey}&language=${language}`;
+const serieRatedUrl = `${apiBaseUrl}/tv/top_rated?api_key=${apiKey}&language=${language}`;
+const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
 
-    const fetchUrl = async (url) => {
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            return data.results;
-        } catch (e) {
-            console.log(e);
-        }
+const fetchUrl = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        return data.results;
+    } catch (e) {
+        console.log(e);
     }
+}
 
-    const createImageUrl = (url) => {
-        return `
-            <img src="${imageBaseUrl}${url.poster_path}" />
+const createImageUrl = (url) => {
+    return `
+        <img src="${imageBaseUrl}${url.poster_path}" />
+    `;
+}
+
+const createMovieList = (movies, element) => {
+    movies.forEach(movie => {
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("item");
+        movieEl.innerHTML = createImageUrl(movie);
+        let year = movie.release_date || movie.first_air_date;
+        let date = new Date(year);
+        let ano = date.getFullYear();
+        movieEl.innerHTML += `
+            <div class="overlay-year">
+                <span>
+                    ${ano}
+                </span>
+            </div>
         `;
-    }
-
-    const createMovieList = (movies, element) => {
-        movies.forEach(movie => {
-            const movieEl = document.createElement("div");
-            movieEl.classList.add("item");
-            movieEl.innerHTML = createImageUrl(movie);
-            let year = movie.release_date || movie.first_air_date;
-            let date = new Date(year);
-            let ano = date.getFullYear();
-            movieEl.innerHTML += `
-                <div class="overlay-year">
-                    <span>
-                        ${ano}
-                    </span>
-                </div>
-            `;
-            movieEl.innerHTML += `
-                <div class="overlay-title">
-                    <h3>${movie.title || movie.name}</h3>
-                </div>
-            `;
-            movieEl.innerHTML += `
-                <button class='btnPlay' type='button'><i class="fas fa-play"></i></button>
-            `;
-            movieEl.addEventListener("click", () => {
-                window.location.href = `./movie/movie.html?id=${movie.id}&type=${movie.title ? "movie" : "serie"}`;
-                console.log(movie.id);
-            });
-            element.appendChild(movieEl);
+        movieEl.innerHTML += `
+            <div class="overlay-title">
+                <h3>${movie.title || movie.name}</h3>
+            </div>
+        `;
+        movieEl.innerHTML += `
+            <button class='btnPlay' type='button'><i class="fas fa-play"></i></button>
+        `;
+        movieEl.addEventListener("click", () => {
+            window.location.href = `./movie/movie.html?id=${movie.id}&type=${movie.title ? "movie" : "serie"}`;
+            console.log(movie.id);
         });
-    }
-
-    const main = async () => {
-        const nowPlayingMovies = await fetchUrl(nowPlayingUrlMovie);
-        const nowPlayingSeries = await fetchUrl(nowPlayingUrlSerie);
-        const topRatedMovies = await fetchUrl(movieRatedUrl);
-        const topRatedSeries = await fetchUrl(serieRatedUrl);
-
-        const nowPlayingMoviesElement = document.getElementById("movies-container");
-        const nowPlayingSeriesElement = document.getElementById("series-container");
-        const topRatedMoviesElement = document.getElementById("toprated-movies-container");
-        const topRatedSeriesElement = document.getElementById("toprated-series-container");
-
-        createMovieList(nowPlayingMovies, nowPlayingMoviesElement);
-        createMovieList(nowPlayingSeries, nowPlayingSeriesElement);
-        createMovieList(topRatedMovies, topRatedMoviesElement);
-        createMovieList(topRatedSeries, topRatedSeriesElement);
-    }
-
-    main();
-
-    document.querySelectorAll('.btn-next').forEach(item => {
-        item.addEventListener('click', event => {
-            const container = item.parentElement.querySelector('.scroll-container');
-            container.scrollLeft += 150;
-        });
-    });
-
-    document.querySelectorAll('.btn-prev').forEach(item => {
-        item.addEventListener('click', event => {
-            const container = item.parentElement.querySelector('.scroll-container');
-            container.scrollLeft -= 150;
-        });
+        element.appendChild(movieEl);
     });
 }
+
+const main = async () => {
+    const nowPlayingMovies = await fetchUrl(nowPlayingUrlMovie);
+    const nowPlayingSeries = await fetchUrl(nowPlayingUrlSerie);
+    const topRatedMovies = await fetchUrl(movieRatedUrl);
+    const topRatedSeries = await fetchUrl(serieRatedUrl);
+
+    const nowPlayingMoviesElement = document.getElementById("movies-container");
+    const nowPlayingSeriesElement = document.getElementById("series-container");
+    const topRatedMoviesElement = document.getElementById("toprated-movies-container");
+    const topRatedSeriesElement = document.getElementById("toprated-series-container");
+
+    createMovieList(nowPlayingMovies, nowPlayingMoviesElement);
+    createMovieList(nowPlayingSeries, nowPlayingSeriesElement);
+    createMovieList(topRatedMovies, topRatedMoviesElement);
+    createMovieList(topRatedSeries, topRatedSeriesElement);
+}
+
+main();
+
+document.querySelectorAll('.btn-next').forEach(item => {
+    item.addEventListener('click', event => {
+        const container = item.parentElement.querySelector('.scroll-container');
+        container.scrollLeft += 150;
+    });
+});
+
+document.querySelectorAll('.btn-prev').forEach(item => {
+    item.addEventListener('click', event => {
+        const container = item.parentElement.querySelector('.scroll-container');
+        container.scrollLeft -= 150;
+    });
+});
